c50a53f41354a162f9250f2067362aa0ccb93d0b
Myhardcore <nik-vna@mail.ru>
2022-02-10 01:03:36 +0300

    2

--- JS EXPERT/complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
+++ JS EXPERT/complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -1,4 +1,4 @@
-// Values and Variables
+/* // Values and Variables
 let country = "Russia";
 const continent = "Eastern Europe";
 let population = 144;
@@ -25,4 +25,32 @@ console.log(language);
 
 console.log(population / 2);
 population++;
-console.log(population);
+console.log(population); */
+
+/* Declare a variable 'numNeighbours' based on a prompt input like this: 
+prompt('How many neighbour countries does your country 
+have?'); 
+2. If there is only 1 neighbour, log to the console 'Only 1 border!' (use loose equality 
+== for now) 
+3. Use an else-if block to log 'More than 1 border' in case 'numNeighbours' 
+is greater than 1 
+4. Use an else block to log 'No borders' (this block will be executed when 
+'numNeighbours' is 0 or any other value) 
+5. Test the code with different values of 'numNeighbours', including 1 and 0. 
+6. Change == to ===, and test the code again, with the same values of 
+'numNeighbours'. Notice what happens when there is exactly 1 border! Why 
+is this happening? 
+7. Finally, convert 'numNeighbours' to a number, and watch what happens now 
+when you input 1 
+8. Reflect on why we should use the === operator and type conversion in this 
+situation */
+
+// let numNeighbours = Number (prompt ('How many neighbour countries does your country have?'))
+
+// if (numNeighbours === 1) {
+//     console.log('Only 1 border!') 
+// } else if (numNeighbours > 1) {
+//     console.log('More than 1 border')
+// } else {
+//     console.log('No borders')
+// }
